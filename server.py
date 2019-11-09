81dc0d16283f6cfaba58716c41fc040d1b806969
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2019-11-09 14:11:54 +0530

    Update server.py

--- server.py
+++ server.py
@@ -19,12 +19,10 @@ def serve_static(filename):
     root_dir = os.path.dirname(os.getcwd())
     return send_from_directory(os.path.join(root_dir, 'static'),   filename)
 
-@app.route('/')
-def home():
-	return send_file('static/index.html')
-
-@app.route('/chat/<bot_id>',methods=['GET', 'POST'])
-def chat(bot_id):
+@app.route('/',methods=['GET', 'POST'])
+def home(bot_id):
+	if bot_id not in table:
+		create_bot()
 	if request.method == 'POST':
 		if bot_id in table:
 			context = table[bot_id]['context']
@@ -41,19 +39,15 @@ def chat(bot_id):
 			bot_im = table[bot_id]["im_url"]
 		return render_template('chat.html',bot=bot_id,bot_im=bot_im)
 
-@app.route('/create_bot',methods=['GET', 'POST'])
 def create_bot():
-
-	bot_id = request.form.get('id')
-	context = request.form.get('context')
-	bot_im = request.form.get('bot_im_url')
+	file = open("knowledgebase.txt","r")
+	bot_id = "GECBSP"
+	context = file.read()
+	bot_im = "https://cdn.mee6.xyz/assets/logo.png"
 	table[bot_id] = {"context":context,"bot_name":bot_id,"im_url":bot_im}
-	return '/chat/'+bot_id
-
 
 
 table = {}
 iq = InferCoQA('model')
-print('done loading model ..')
 
-app.run()
+app.run(debug=True)
