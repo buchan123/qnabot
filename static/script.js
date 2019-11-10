81c7a8691aa674303528a48b228e5369c23b8794
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2019-11-10 19:57:24 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -9,7 +9,7 @@ $(function() {
     if(msg.trim() == ''){
       return false;
     }
-    generate_message(msg, 'self');
+    generate_message(msg, 'self',"<img src='https://bootdey.com/img/Content/avatar/avatar2.png'>");
     var buttons = [
         {
           name: 'Existing User',
@@ -31,19 +31,19 @@ $(function() {
           };
           answer = ms.text
           
-      generate_message(answer, 'user');  
+      generate_message(answer, 'user',"https://cdn.mee6.xyz/assets/logo.png");  
         });
 
     }, 1000)
     
   })
   
-  function generate_message(msg, type) {
+  function generate_message(msg, type,avatar) {
     INDEX++;
     var str="";
     str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg "+type+"\">";
     str += "          <span class=\"msg-avatar\">";
-    str += "            <img src='https://bootdey.com/img/Content/avatar/avatar2.png'>";
+    str += avatar;
     str += "          <\/span>";
     str += "          <div class=\"cm-msg-text\">";
     str += msg;
@@ -56,43 +56,6 @@ $(function() {
     }    
     $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);    
   }  
-  
-  function generate_button_message(msg, buttons){    
-    /* Buttons should be object array 
-      [
-        {
-          name: 'Existing User',
-          value: 'existing'
-        },
-        {
-          name: 'New User',
-          value: 'new'
-        }
-      ]
-    */
-    INDEX++;
-    var btn_obj = buttons.map(function(button) {
-       return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
-    }).join('');
-    var str="";
-    str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
-    str += "          <span class=\"msg-avatar\">";
-    str += "            <img src=\"https:\/\/image.crisp.im\/avatar\/operator\/196af8cc-f6ad-4ef7-afd1-c45d5231387c\/240\/?1483361727745\">";
-    str += "          <\/span>";
-    str += "          <div class=\"cm-msg-text\">";
-    str += msg;
-    str += "          <\/div>";
-    str += "          <div class=\"cm-msg-button\">";
-    str += "            <ul>";   
-    str += btn_obj;
-    str += "            <\/ul>";
-    str += "          <\/div>";
-    str += "        <\/div>";
-    $(".chat-logs").append(str);
-    $("#cm-msg-"+INDEX).hide().fadeIn(300);   
-    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
-    $("#chat-input").attr("disabled", true);
-  }
   
   $(document).delegate(".chat-btn", "click", function() {
     var value = $(this).attr("chat-value");
